4a74f468d6aba21d50c1b4452e2a4390193ce027
raghava999 <raghava.c25@gmail.com>
2020-09-04 11:59:20 -0400

    Spring Profiles

--- src/main/java/com/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/com/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package com.springframework.sfgdi;
 
-import com.springframework.sfgdi.controllers.ConstructorInjectedController;
-import com.springframework.sfgdi.controllers.MyController;
-import com.springframework.sfgdi.controllers.PropertyInjectedController;
-import com.springframework.sfgdi.controllers.SetterInjectedController;
+import com.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +11,10 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext ctx =  SpringApplication.run(SfgDiApplication.class, args);
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println("PROFILES");
+		System.out.println(i18nController.sayHello());
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
